02109d7bcc7c23382c5a009b08663c74760397d1
Alejandro Estringana Ruiz <estringana@gmail.com>
2017-01-15 00:39:45 +1100

    Add the migration which created the standards table

---
+++ StandardiseIt/database/migrations/2017_01_14_123130_create_standards_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStandardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('standards', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('title');
+			$table->longText('proposition');
+			$table->dateTime('proposed_at');
+			$table->string('status');
+			$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('standards');
+    }
+}
